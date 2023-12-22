fff4fe4abc2ef8996f82abd78867d5e3a7f9171e
Je Hon Tan <jehontan@gmail.com>
2023-12-22 16:07:19 +0800

    bugfixes

--- src/listener.cpp
+++ src/listener.cpp
@@ -5,34 +5,27 @@
 #include <tf2_ros/buffer.h>
 #include <tf2_ros/transform_listener.h>
 
-
 int main(int argc, char* argv[])
 {
     ros::init(argc, argv, "tf_relay_listener");
-    ros::NodeHandle nh;
+    ros::NodeHandle nh("~");
 
     float hz = nh.param<float>("hz", 1.0);
-    std::string pose_topic, transform_topic, source_frame, target_frame;
+    std::string pose_topic, transform_topic, reference_frame, child_frame;
 
-    nh.getParam("source_frame", source_frame);
-    nh.getParam("target_frame", target_frame);
+    nh.getParam("reference_frame", reference_frame);
+    nh.getParam("child_frame", child_frame);
 
     bool pub_pose = nh.param<bool>("pub_pose", false);
     bool pub_transform = nh.param<bool>("pub_transform", true);
 
     ros::Publisher pose_pub, transform_pub;
 
-    if (pub_pose)
-    {
-        nh.getParam("pose_topic", pose_topic);
-        pose_pub = nh.advertise<geometry_msgs::PoseStamped>(pose_topic, 10);
-    }
+    pose_topic = nh.param<std::string>("pose_topic", "pose");
+    transform_topic = nh.param<std::string>("transform_topic", "transform");
 
-    if (pub_transform)
-    {
-        nh.getParam("transform_topic", transform_topic);
-        transform_pub = nh.advertise<geometry_msgs::TransformStamped>(transform_topic, 10);
-    }
+    pose_pub = nh.advertise<geometry_msgs::PoseStamped>(pose_topic, 10);
+    transform_pub = nh.advertise<geometry_msgs::TransformStamped>(transform_topic, 10);
 
     tf2_ros::Buffer tf_buf;
     tf2_ros::TransformListener tf_listener(tf_buf);
@@ -46,22 +39,31 @@ int main(int argc, char* argv[])
     {
         ros::Time stamp = ros::Time::now();
 
-        transform_msg = tf_buf.lookupTransform(target_frame, source_frame, stamp);
-
-        if (pub_pose)
+        try
         {
-            pose_msg.header = transform_msg.header;
-            pose_msg.pose.orientation = transform_msg.transform.rotation;
-            pose_msg.pose.position.x = transform_msg.transform.translation.x;
-            pose_msg.pose.position.y = transform_msg.transform.translation.y;
-            pose_msg.pose.position.z = transform_msg.transform.translation.z;
+            transform_msg = tf_buf.lookupTransform(reference_frame, child_frame, stamp);
+            if (pub_pose)
+            {
+                pose_msg.header = transform_msg.header;
+                pose_msg.pose.orientation = transform_msg.transform.rotation;
+                pose_msg.pose.position.x = transform_msg.transform.translation.x;
+                pose_msg.pose.position.y = transform_msg.transform.translation.y;
+                pose_msg.pose.position.z = transform_msg.transform.translation.z;
 
-            pose_pub.publish(pose_msg);
+                pose_pub.publish(pose_msg);
+            }
+
+            if (pub_transform)
+            {
+                transform_pub.publish(transform_msg);
+            }
+        }
+        catch(tf2::LookupException e)
+        {
+            ROS_WARN(e.what());
         }
 
-        if (pub_transform)
-            pose_pub.publish(transform_msg);
-
+        ros::spinOnce();
         rate.sleep();
     }
 
