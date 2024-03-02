fd28458905c4c9caefbb081014029d61afee6576
Je Hon Tan <jehontan@gmail.com>
2024-03-03 02:22:36 +0800

    handle case of missing TF lookup

--- src/listener.cpp
+++ src/listener.cpp
@@ -1,6 +1,7 @@
 #include <ros/ros.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <geometry_msgs/TransformStamped.h>
+#include <nav_msgs/Odometry.h>
 
 #include <tf2_ros/buffer.h>
 #include <tf2_ros/transform_listener.h>
@@ -11,21 +12,24 @@ int main(int argc, char* argv[])
     ros::NodeHandle nh("~");
 
     float hz = nh.param<float>("hz", 1.0);
-    std::string pose_topic, transform_topic, reference_frame, child_frame;
+    std::string reference_frame, child_frame;
 
     nh.getParam("reference_frame", reference_frame);
     nh.getParam("child_frame", child_frame);
 
     bool pub_pose = nh.param<bool>("pub_pose", false);
     bool pub_transform = nh.param<bool>("pub_transform", true);
+    bool pub_odom = nh.param<bool>("pub_odom", false);
 
-    ros::Publisher pose_pub, transform_pub;
+    ros::Publisher pose_pub, transform_pub, odom_pub;
 
-    pose_topic = nh.param<std::string>("pose_topic", "pose");
-    transform_topic = nh.param<std::string>("transform_topic", "transform");
+    std::string pose_topic = nh.param<std::string>("pose_topic", "pose");
+    std::string transform_topic = nh.param<std::string>("transform_topic", "transform");
+    std::string odom_topic = nh.param<std::string>("odom_topic", "odom");
 
     pose_pub = nh.advertise<geometry_msgs::PoseStamped>(pose_topic, 10);
     transform_pub = nh.advertise<geometry_msgs::TransformStamped>(transform_topic, 10);
+    odom_pub = nh.advertise<nav_msgs::Odometry>(odom_topic, 10);
 
     tf2_ros::Buffer tf_buf;
     tf2_ros::TransformListener tf_listener(tf_buf);
@@ -34,31 +38,52 @@ int main(int argc, char* argv[])
 
     geometry_msgs::TransformStamped transform_msg;
     geometry_msgs::PoseStamped pose_msg;
+    nav_msgs::Odometry odom_msg;
 
     while (ros::ok())
     {
-        try
+        if (tf_buf.canTransform(reference_frame, child_frame, ros::Time(0)))
         {
-            transform_msg = tf_buf.lookupTransform(reference_frame, child_frame, ros::Time(0));
-            if (pub_pose)
+            try
             {
-                pose_msg.header = transform_msg.header;
-                pose_msg.pose.orientation = transform_msg.transform.rotation;
-                pose_msg.pose.position.x = transform_msg.transform.translation.x;
-                pose_msg.pose.position.y = transform_msg.transform.translation.y;
-                pose_msg.pose.position.z = transform_msg.transform.translation.z;
+                transform_msg = tf_buf.lookupTransform(reference_frame, child_frame, ros::Time(0));
 
-                pose_pub.publish(pose_msg);
+                if (pub_pose)
+                {
+                    pose_msg.header = transform_msg.header;
+                    pose_msg.pose.orientation = transform_msg.transform.rotation;
+                    pose_msg.pose.position.x = transform_msg.transform.translation.x;
+                    pose_msg.pose.position.y = transform_msg.transform.translation.y;
+                    pose_msg.pose.position.z = transform_msg.transform.translation.z;
+
+                    pose_pub.publish(pose_msg);
+                }
+
+                if (pub_transform)
+                {
+                    transform_pub.publish(transform_msg);
+                }
+
+                if (pub_odom)
+                {
+                    odom_msg.header = transform_msg.header;
+                    odom_msg.child_frame_id = transform_msg.child_frame_id;
+                    odom_msg.pose.pose.orientation = transform_msg.transform.rotation;
+                    odom_msg.pose.pose.position.x = transform_msg.transform.translation.x;
+                    odom_msg.pose.pose.position.y = transform_msg.transform.translation.y;
+                    odom_msg.pose.pose.position.z = transform_msg.transform.translation.z;
+
+                    odom_pub.publish(odom_msg);
+                }
             }
-
-            if (pub_transform)
+            catch(tf2::TransformException e)
             {
-                transform_pub.publish(transform_msg);
+                ROS_WARN(e.what());
             }
         }
-        catch(tf2::TransformException e)
+        else
         {
-            ROS_WARN(e.what());
+            ROS_WARN_THROTTLE(5, "No tf %s -> %s. May not be available yet.", reference_frame.c_str(), child_frame.c_str());
         }
 
         ros::spinOnce();
